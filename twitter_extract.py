382c62150594079c389e7efb230e360a8703d4f6
Dikshant Devkota <ddevkota42@gmail.com>
2022-03-24 07:47:54 +0000

    twitter data

---
+++ twitter_extract.py
@@ -0,0 +1,37 @@
+import json
+import csv
+import tweepy
+from tweepy import OAuthHandler
+
+# authentication keys required to access the tweets
+consumer_key = 'A7qvVgeq5w4reI7YE4fTXLpoS'
+consumer_secret = 'C0Cc2UEroDjuDgGZm6tO1iThdOTOCD6UXq59DKI0aHO5Yd3V3W'
+access_token = '1501541633610944515-SqjjlQS0liX8du52Osl9URT94QTWwt'
+access_token_secret = 'fBVNDULOae9OCs46fbxCjzQR0yJ2kbxzBZqSotpRETyUb'
+
+# create OAuthHandler object
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# set access token and secret
+auth.set_access_token(access_token, access_token_secret)
+
+# create tweepy API object to fetch tweets
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+def get_tweets():
+    # Open/create a file to append data to
+    csvFile = open('bitcoin_tweets.csv', 'a')
+
+    #Use csv writer
+    csvWriter = csv.writer(csvFile)
+    # startDate = datetime.datetime(2020, 1, 1, 0, 0, 0)
+    # endDate =   datetime.datetime(2012, 1, 1, 0, 0, 0)
+    # call twitter api to fetch tweets
+    fetched_tweets = tweepy.Cursor(api.search_tweets,q=('bitcoin'), lang = "en").items(2000)
+    for tweet in fetched_tweets:
+        # Write a row to the CSV file. I use encode UTF-8
+        csvWriter.writerow([tweet.id, tweet.user.screen_name, tweet.user.name, tweet.user.url, tweet.created_at, tweet.favorite_count, tweet.retweet_count, tweet.text])
+        
+
+    return fetched_tweets
+
+tweets = get_tweets()
