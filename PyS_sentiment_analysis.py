463affa6814b838f9d2c97f12ed8eb88961a24a8
NightCrawler15 <ddevkota42@gmail.com>
2022-04-20 11:31:53 +0200

    Made changes to PySpark

--- PyS_sentiment_analysis.py
+++ PyS_sentiment_analysis.py
@@ -12,59 +12,54 @@ sql = SQLContext(sc)
 
 from textblob import Blobber
 from textblob.sentiments import NaiveBayesAnalyzer
+tweets_sentiments = {"Negative": 0, "Positive": 0, "Neutral": 0}
 
-class SentimentAnalyzer():
-    def __init__(self, file_location):
-        self.tweets_sentiments = {"Negative": 0, "Positive": 0, "Neutral": 0}
-        self.__load_data(file_location)
+def clean_data(txt):
+    # Remove mentions
+    txt = re.sub(r'@[A-Za-z0-9_]+', '', txt)
+    # Remove hashtags
+    txt = re.sub(r'#[A-Z0-9]+', '', txt)
+    # Remove retweets:
+    txt = re.sub(r'RT : ', '', txt)
+    # Remove urls
+    txt = re.sub(r'https?:\/\/[A-Za-z0-9\.\/]+', '', txt)
+    #remove amp
+    txt = re.sub(r'&amp;', '', txt)
+    #rempve strange characters
+    txt = re.sub(r'ðŸ™', '', txt)
+    #remove new lines
+    txt = re.sub(r'\n', ' ', txt)
+    #converting lower text
+    txt = txt.lower()
+    return txt
 
-    def __load_data(self, file_location):
-        tweets_df = sql.read.csv(file_location, header=False, inferSchema= True)
-        # tweets_df.show()
-        tweets_analyze = self.__analyze_text
-        tweets_df.rdd.foreach(lambda x: tweets_analyze(x['_c1']))
-        return
+def load_data(file_location):
+    tweets_df = sql.read.csv(file_location, header=False, inferSchema= True)
+    # tweets_df.show()
+    tweets_df.rdd.foreach(lambda x: analyze_text(x['_c1']))
+    return
 
-    def __analyze_text(self, txt):
-        # sentiments = {"Negative": 0, "Positive": 0, "Neutral": 0}
-        self.tweets_sentiments[self.__analyzer(txt)] += 1
-        return
+def analyze_text(txt):
+    # sentiments = {"Negative": 0, "Positive": 0, "Neutral": 0}
+    global tweets_sentiments
+    tweets_sentiments[analyzer(txt)] += 1
+    return
 
-    def __clean_data(self, txt):
-        # Remove mentions
-        txt = re.sub(r'@[A-Za-z0-9_]+', '', txt)
-        # Remove hashtags
-        txt = re.sub(r'#[A-Z0-9]+', '', txt)
-        # Remove retweets:
-        txt = re.sub(r'RT : ', '', txt)
-        # Remove urls
-        txt = re.sub(r'https?:\/\/[A-Za-z0-9\.\/]+', '', txt)
-        #remove amp
-        txt = re.sub(r'&amp;', '', txt)
-        #rempve strange characters
-        txt = re.sub(r'ðŸ™', '', txt)
-        #remove new lines
-        txt = re.sub(r'\n', ' ', txt)
-        #converting lower text
-        txt = txt.lower()
-        return txt
+def analyzer(txt):
+    # Initializing Native Bayes analyzer
+    # Sentiment intensity analyser uses Naiive Bayes 
+    blob = Blobber(analyzer=NaiveBayesAnalyzer())
+    # passing the filtered data and
+    # classifying into positive/ negative using naive bayes
+    classifier = blob(clean_data(txt)).sentiment
 
-    def __analyzer(self, txt):
-        # Initializing Native Bayes analyzer
-        # Sentiment intensity analyser uses Naiive Bayes 
-        blob = Blobber(analyzer=NaiveBayesAnalyzer())
-        # passing the filtered data and
-        # classifying into positive/ negative using naive bayes
-        classifier = blob(self.__clean_data(txt)).sentiment
-
-        if round(classifier[1], 2) > round(classifier[2], 2):
-            return "Positive"
-        elif round(classifier[1], 2) < round(classifier[2], 2):
-            return "Negative"
-        else:
-            return "Neutral"
+    if round(classifier[1], 2) > round(classifier[2], 2):
+        return "Positive"
+    elif round(classifier[1], 2) < round(classifier[2], 2):
+        return "Negative"
+    else:
+        return "Neutral"
 
 if __name__ == '__main__':
-    spark_sentiment_analyzer = SentimentAnalyzer("hdfs://namenode:9000/group17_sentiment_analysis_on_tweets/covid_tweets_clean.csv")
-    sentiments_df = spark_sentiment_analyzer.tweets_sentiments
-    print(type(sentiments_df), sentiments_df)
+    load_data("hdfs://namenode:9000/group17_sentiment_analysis_on_tweets/covid_tweets_clean.csv")
+    print(tweets_sentiments)
--- coulmn_merge.py
+++ coulmn_merge.py
@@ -2,11 +2,6 @@ import csv
 import re
 import pandas as pd
 import os.path
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-TWEETS = os.path.join(BASE_DIR, "tweets.csv")
-# tweets = pandas.read_csv(TWEETS, sep = ';', header = None, on_bad_lines='skip',lineterminator='\n')
-# tweets[0]
-count = 0
 
 '''df = pd.read_csv('train.csv', encoding = "ISO-8859-1")
 with open("train_clean.csv", "w") as result:
@@ -18,18 +13,14 @@ with open("train_clean.csv", "w") as result:
     #print(r[0], r[1], r[2], r[3], r[4], r[6], r[7], r[8])
         polarity = "negative" if r[0] == "0" else "positive" if r[0] == "4" else "neutral"
         writer.writerow((r[5], polarity))'''
-with open("covid_tweets.csv", "r") as source:
-    reader = csv.reader(source)
-    count = 0
-      
-    with open("covid_tweets_clean.csv", "w") as result:
-        writer = csv.writer(result)
-        for r in reader:
-            
+
+df = pd.read_csv('covid_tweets.csv', encoding = "ISO-8859-1")      
+with open("covid_tweets_clean.csv", "w") as result:
+	writer = csv.writer(result)
+        # for r in reader:
+	for index, r in df.iterrows():    
             # Use CSV Index to remove a column from CSV
             #r[3] = r['url']
             tweet_id = r[0]
             text = re.sub(r'\n', ' ', r[6])
             writer.writerow((tweet_id, text))
-            if (count > 5): break
-            count += 1
